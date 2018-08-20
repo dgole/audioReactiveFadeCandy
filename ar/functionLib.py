2fef079b88bf910a4cb1348ca3831970b4e9b746
dgole <dgole100@gmail.com>
2018-08-20 13:47:19 -0600

    Update functionLib.py

--- ar/functionLib.py
+++ ar/functionLib.py
@@ -12,7 +12,7 @@ class Pixels():
         self.nLed       = nStrips*lStrip
         self.minDisplay = minDisplay
         self.array      = np.zeros([self.nLed, 3])
-	def update(self, arrayNew, alphaRise, alphaDecay):
+    def update(self, arrayNew, alphaRise, alphaDecay):
         if arrayNew.shape == (self.lStrip, 3):
             arrayNew = np.tile(arrayNew, (self.nStrips, 1))
             alpha = arrayNew - self.array
@@ -22,9 +22,9 @@ class Pixels():
         elif arrayNew.shape == (self.nLed,   3):
             alpha = arrayNew - self.array
             alpha[alpha > 0.0 ] = alphaRise
-		    alpha[alpha <= 0.0] = alphaDecay
+            alpha[alpha <= 0.0] = alphaDecay
             self.array = alpha*arrayNew + (1.0-alpha)*self.array
-	def getArrayForDisplay(self):
-		returnArray = self.array
-		returnArray[returnArray < minDisplay] = 0
-		return returnArray
+    def getArrayForDisplay(self):
+        returnArray = self.array
+        returnArray[returnArray < minDisplay] = 0
+        return returnArray
