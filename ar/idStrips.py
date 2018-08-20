a0ba2ce952d4dc1378b4e1965145ec2dca76b704
dgole <dgole100@gmail.com>
2018-08-20 13:48:17 -0600

    Update idStrips.py

--- ar/idStrips.py
+++ ar/idStrips.py
@@ -8,16 +8,16 @@ nStrips = 8
 lStrip  = 64
 client = fastopc.FastOPC('localhost:7890')
 
-pixels = Pixels(nStrips, lStrip, 0)
+pixels = lib.Pixels(nStrips, lStrip, 0)
 
 theo = np.zeros_like(pixels.array)
 for i in range(0, nStrips):
-	base=lStrip*i
-	arrayTheo[base:base+i+1]  = [0,0,255]
+    base=lStrip*i
+    arrayTheo[base:base+i+1]  = [0,0,255]
 
 while True:
-	pixels.update(arrayTheo, 1.0, 0.0)
-	client.putPixels(0, pixels.getArrayForDisplay())
-	time.sleep(1)
-	client.putPixels(0, np.zeros_like(pixels.getArrayForDisplay()))
-	time.sleep(1)
+    pixels.update(arrayTheo, 1.0, 0.0)
+    client.putPixels(0, pixels.getArrayForDisplay())
+    time.sleep(1)
+    client.putPixels(0, np.zeros_like(pixels.getArrayForDisplay()))
+    time.sleep(1)
