b8d3074ae590520b26dd0ce18e18a85c7c767fe6
dgole <dgole100@gmail.com>
2018-08-20 13:52:38 -0600

    Update idStrips.py

--- ar/idStrips.py
+++ ar/idStrips.py
@@ -19,5 +19,6 @@ while True:
     pixels.update(theo, 1.0, 1.0)
     client.putPixels(0, pixels.getArrayForDisplay())
     time.sleep(1)
-    client.putPixels(0, np.zeros_like(pixels.getArrayForDisplay()))
+    pixels.update(np.zeros_like(theo), 1.0, 1.0)
+    client.putPixels(0, pixels.getArrayForDisplay())
     time.sleep(1)
