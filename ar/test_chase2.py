ec1819ade5d9e7a214e5860af9cc2f26f9795e2b
dgole <dgole100@gmail.com>
2018-08-20 23:41:13 -0600

    Create test_chase2.py

---
+++ ar/test_chase2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Light each LED in sequence, and repeat.
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+
+nStrips = 8
+lStrip  = 64
+
+client = fastopc.FastOPC('localhost:7890')
+
+n = 1
+dir = 1
+pixels = lib.Pixels(nStrips, lStrip, 20)
+theoStrip = np.zeros([lStrip*nStrips,3])
+color=2
+theoStrip[n,color] = 255
+
+while True:
+    if n == (511):
+        dir *=-1
+    elif n == 0:
+        dir*=-1
+        theoStrip=np.roll(theoStrip, 1, axis=1)
+    theoStrip = np.roll(theoStrip, dir, axis=0)
+    pixels.update(theoStrip, 0.7, 0.01)
+    client.putPixels(0, pixels.getArrayForDisplay())
+    n+=dir
+    time.sleep(0.001)
