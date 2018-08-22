f53918ee1abab019850ed1c0ff2ec0ad7f0607f2
dgole <dgole100@gmail.com>
2018-08-21 20:28:51 -0600

    Update test_ar.py

--- ar/test_ar.py
+++ ar/test_ar.py
@@ -11,15 +11,14 @@ nStrips = 16
 lStrip  = 64
 client = fastopc.FastOPC('localhost:7890')
 
-pixels = Pixels(nStrips, lStrip, 20)
-theo = np.zeros_like(pixels.array)
+pixels    = Pixels(nStrips, lStrip, 20)
+theoStrip = np.zeros(lStrip, 3)
 
 stream = micStream.Stream()
 
 while True:
     stream.readAndCalculate()
-	pixels.update(arrayTheo, 1.0, 0.0)
+    power = stream.notesSpectrum(10)
+    theoStrip[:,2] = 255
+	pixels.update(theoStrip, 0.5, 0.1)
 	client.putPixels(0, pixels.getArrayForDisplay())
-	time.sleep(1)
-	client.putPixels(0, np.zeros_like(pixels.getArrayForDisplay()))
-	time.sleep(1)
