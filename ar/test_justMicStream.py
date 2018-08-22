b24fe62310cd89659e293c8d2722598772f41f08
dgole <dgole100@gmail.com>
2018-08-22 13:11:18 -0600

    Create test_justMicStream.py

---
+++ ar/test_justMicStream.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+import micStream
+
+nStrips = 16
+lStrip  = 64
+client = fastopc.FastOPC('localhost:7890')
+
+pixels    = lib.Pixels(nStrips, lStrip, 20)
+theoStrip = np.zeros([lStrip, 3])
+
+theoStrip[:,2] = 100
+pixels.update(theoStrip, 1.0, 0.1)
+client.putPixels(0, pixels.getArrayForDisplay())
+
+stream = micStream.Stream(fps=10, nBuffers=2)
+print(stream.fps)
+print(stream.framesPerBuffer)
+
+print("marker0")
+while True:
+    print("marker1")
+    stream.readNewData()
+    print("marker2")
+    #if success:
+        #print("marker3")
+        #power = stream.noteSpectrum[10]
+        #print(power)
+        #print("marker4")
