9362d6982b4da9175e19449092587c8d821ca9f0
dgole <dgole100@gmail.com>
2018-08-21 21:26:52 -0600

    Update micStream.py

--- ar/micStream.py
+++ ar/micStream.py
@@ -113,8 +113,8 @@ class Stream:
         try:
             self.newMicData = np.fromstring(self.stream.read(self.framesPerBuffer), dtype=np.int16)
             self.newMicData = self.newMicData.astype(np.float32)
-            self.micData = np.roll(self.micData, -self.framesPerBuffer)
-            self.micData[(self.nBuffers-1)*self.framesPerBuffer:(self.nBuffers)*self.framesPerBuffer] = self.newMicData
+            #self.micData = np.roll(self.micData, -self.framesPerBuffer)
+            #self.micData[(self.nBuffers-1)*self.framesPerBuffer:(self.nBuffers)*self.framesPerBuffer] = self.newMicData
             print('successfully got data from audio stream')
             self.frameCount += 1
             returnVal=True
