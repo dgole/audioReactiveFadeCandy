011bdb29745c832f9ab9047b51128e8980a298b2
dgole <dgole100@gmail.com>
2018-08-23 14:47:19 -0600

    Create ar_bassWiden.py

---
+++ ar/ar_bassWiden.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+import micStream
+
+nStrips = 16
+lStrip  = 64
+client = fastopc.FastOPC('localhost:7890')
+
+pixels    = lib.Pixels(nStrips, lStrip, 0)
+theoStrip = np.zeros([lStrip, 3])
+
+stream = micStream.Stream(fps=40, nBuffers=4)
+
+powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
+
+while True:
+    success = stream.readAndCalc()
+    if success: 
+        power = np.sum(stream.freqSpectrum[10//5:300//5])
+        powerSmooth.update(power)
+        displayPower = int(150*power/powerSmooth.value)
+        theoStrip[:,0] = displayPower
+        pixels.update(theoStrip, 0.9, 0.2)
+        print(displayPower)
+        client.putPixels(0, pixels.getArrayForDisplay())
