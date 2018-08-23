697b4725dec7d1d19f996d37e6f19964a7f3fd57
dgole <dgole100@gmail.com>
2018-08-23 16:34:18 -0600

    Update nar_stars.py

--- ar/nar_stars.py
+++ ar/nar_stars.py
@@ -11,6 +11,9 @@ lStrip  = 64
 
 client = fastopc.FastOPC('localhost:7890')
 
+waitTime = 0.1
+#fade stars in over 10 frames at 0.1 s/frame is 1s per star
+
 nStars = 200
 pixels = lib.Pixels(nStrips, lStrip, 0)
 theoStrips = np.zeros([nStars, nStrips*lStrip, 3])
@@ -22,14 +25,16 @@ while True:
     colors    = np.random.randint(0, 255, size=[nStars,3])
     for n in range(nStars): theoStrips[n, positions[n]] = colors[n] 
     # bring in new stars
-    for i in range(0,nStars*50):
-        starNum = np.floor(i/100)
+    for i in range(0,nStars*10):
+        starNum = np.floor(i/10)
         print(starNum)
-        pixels.update(theoStrips[starNum], 0.05, 0.0)
+        pixels.update(theoStrips[starNum], 0.2, 0.0)
         client.putPixels(0, pixels.getArrayForDisplay())
+        time.sleep(waitTime)
     # clear old stars
-    for i in range(0,500):
+    for i in range(0,int(20/waitTime)):
         pixels.update(zeroStrip, 0.0, 0.02)
         client.putPixels(0, pixels.getArrayForDisplay())
+        time.sleep(waitTime)
     # fully zero out strip
     pixels.update(zeroStrip, 1.0, 1.0)
