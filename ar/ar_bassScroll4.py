cf6f004d841dbcf0766d82c54ea08736442b0e3e
dgole <dgole100@gmail.com>
2018-08-23 15:28:27 -0600

    Create ar_bassScroll4.py

---
+++ ar/ar_bassScroll4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+import micStream
+
+nStrips = 16
+lStrip  = 64
+client = fastopc.FastOPC('localhost:7890')
+
+pixels    = lib.Pixels(nStrips, lStrip, 0)
+theo      = np.zeros([nStrips*lStrip, 3])
+
+stream = micStream.Stream(fps=40, nBuffers=4)
+
+powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
+nColorWheel = 300
+colorWheel = lib.getColorWheel(nColorWheel)
+frameCount = 0
+
+while True:
+    success = stream.readAndCalc()
+    if success:
+        frameNumEff = np.mod(frameCount, nColorWheel)
+        power = np.sum(stream.freqSpectrum[10//5:300//5])
+        powerSmooth.update(power)
+        displayPower = int(122*power/powerSmooth.value)
+        theoStrip = np.roll(theoStrip, 128, axis=0)
+        theoStrip[0:128] = displayPower * colorWheel[frameNumEff]
+        pixels.update(theoStrip, 1.0, 0.5)
+        #print(displayPower * colorWheel[frameNumEff])
+        client.putPixels(0, pixels.getArrayForDisplay())
+        frameCount+=1
