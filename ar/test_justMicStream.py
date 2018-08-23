4a84e2b849c3eb0bf227a40998bf6d85c6ab61df
dgole <dgole100@gmail.com>
2018-08-22 21:36:21 -0600

    Update test_justMicStream.py

--- ar/test_justMicStream.py
+++ ar/test_justMicStream.py
@@ -23,7 +23,7 @@ import pyaudio
 CHUNK = 8192    # input buffer size in frames
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATES = (96000, 88200, 48000, 44100, 32000, 24000)
+RATES = [96000, 88200, 48000, 44100, 32000, 24000]
 # open sound card data stream
 npoints = 1000000
 for rate in RATES:
