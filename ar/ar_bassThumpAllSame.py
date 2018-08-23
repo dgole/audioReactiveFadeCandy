300a71cd02a8316d651c31de764bcc5df7fcfddf
dgole <dgole100@gmail.com>
2018-08-23 14:26:39 -0600

    Update ar_bassThumpAllSame.py

--- ar/ar_bassThumpAllSame.py
+++ ar/ar_bassThumpAllSame.py
@@ -7,20 +7,22 @@ import micStream
 
 nStrips = 16
 lStrip  = 64
-client = fastopc.FastOPC('localhost:7890')
+#client = fastopc.FastOPC('localhost:7890')
 
 pixels    = lib.Pixels(nStrips, lStrip, 0)
 theoStrip = np.zeros([lStrip, 3])
 
 stream = micStream.Stream(fps=40, nBuffers=4)
 
+powerSmooth = functionLib.ExpFilter(val=0.1, alpha_rise=0.1, alpha_decay=0.1)
+
 while True:
     success = stream.readAndCalc()
     if success: 
         power = np.sum(stream.freqSpectrum[30//5:300//5])
-        print(power)
-        print(stream.freqs)
-        print(stream.notes)
-        theoStrip[:,0] = power/1.0
+        powerSmooth.update(power)
+        displayPower = power/powerSmooth
+        theoStrip[:,0] = displayPower
         pixels.update(theoStrip, 0.9, 0.2)
-        client.putPixels(0, pixels.getArrayForDisplay())
+        print(power/powerSmooth
+        #client.putPixels(0, pixels.getArrayForDisplay())
