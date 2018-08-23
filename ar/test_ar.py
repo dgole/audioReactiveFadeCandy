0c1983e077bbcad894679e059f6e9d130c5a2bb1
dgole <dgole100@gmail.com>
2018-08-22 23:01:24 -0600

    Update test_ar.py

--- ar/test_ar.py
+++ ar/test_ar.py
@@ -12,12 +12,12 @@ client = fastopc.FastOPC('localhost:7890')
 pixels    = lib.Pixels(nStrips, lStrip, 0)
 theoStrip = np.zeros([lStrip, 3])
 
-stream = micStream.Stream(fps=40, nBuffers=8)
+stream = micStream.Stream(fps=40, nBuffers=4)
 
 while True:
     success = stream.readAndCalc()
     if success:
-        power = np.sum(stream.noteSpectrum[0:20])
+        power = np.sum(stream.freqSpectrum[30//5:120//5])
         print(power)
         print(stream.freqs)
         print(stream.notes)
