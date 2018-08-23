baf91e098debcacf566237591b72afd1b6cff323
dgole <dgole100@gmail.com>
2018-08-23 16:39:59 -0600

    Update nar_stars.py

--- ar/nar_stars.py
+++ ar/nar_stars.py
@@ -23,11 +23,14 @@ while True:
     # select new values
     positions = np.random.randint(0, high=nStrips*lStrip-1, size=nStars)
     colors    = np.random.randint(0, 255, size=[nStars,3])
+    for n in range(nStars)
+    maxColor  = np.amax(colors[n])
+    colors[n]    = 255*colors[n]/maxColor
+    print(colors)
     for n in range(nStars): theoStrips[n, positions[n]] = colors[n] 
     # bring in new stars
     for i in range(0,nStars*10):
         starNum = np.floor(i/10)
-        print(starNum)
         pixels.update(theoStrips[starNum], 0.2, 0.0)
         client.putPixels(0, pixels.getArrayForDisplay())
         time.sleep(waitTime)
