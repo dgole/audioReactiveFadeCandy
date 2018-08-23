24cbfbc47ab41e00b304c419030d759f5945314a
dgole <dgole100@gmail.com>
2018-08-22 20:23:41 -0600

    Update test_justMicStream.py

--- ar/test_justMicStream.py
+++ ar/test_justMicStream.py
@@ -5,18 +5,18 @@ import numpy as np
 import functionLib as lib
 import micStream
 
-nStrips = 16
-lStrip  = 64
-client = fastopc.FastOPC('localhost:7890')
+#nStrips = 16
+#lStrip  = 64
+#client = fastopc.FastOPC('localhost:7890')
 
-pixels    = lib.Pixels(nStrips, lStrip, 20)
-theoStrip = np.zeros([lStrip, 3])
+#pixels    = lib.Pixels(nStrips, lStrip, 20)
+#theoStrip = np.zeros([lStrip, 3])
 
-theoStrip[:,2] = 100
-pixels.update(theoStrip, 1.0, 0.1)
-client.putPixels(0, pixels.getArrayForDisplay())
+#theoStrip[:,2] = 100
+#pixels.update(theoStrip, 1.0, 0.1)
+#client.putPixels(0, pixels.getArrayForDisplay())
 
-stream = micStream.Stream(fps=10, nBuffers=2)
+stream = micStream.Stream(fps=40, nBuffers=4)
 print(stream.fps)
 print(stream.framesPerBuffer)
 
