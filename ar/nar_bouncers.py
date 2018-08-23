56fea805c079d40f78b198d5a985050910b1788a
dgole <dgole100@gmail.com>
2018-08-23 11:43:02 -0600

    Update nar_bouncers.py

--- ar/nar_bouncers.py
+++ ar/nar_bouncers.py
@@ -49,7 +49,7 @@ for i in range(32):
 
 while True:
     for i in range(0,nBouncers):
-        stripNum = np.mod(nBouncer, nStrips)
+        stripNum = np.mod(i, nStrips)
         base = stripNum*lStrip
         theoStrip[base:base+lStrip] = bouncerList[i].getFullOutArray()
         bouncerList[i].update()
