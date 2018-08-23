b085912f6af52158e33d70a5d2e5abcd520c5196
dgole <dgole100@gmail.com>
2018-08-23 17:50:56 -0600

    Update nar_bouncersWave.py

--- ar/nar_bouncersWave.py
+++ ar/nar_bouncersWave.py
@@ -42,25 +42,24 @@ class Bouncer :
 pixels = lib.Pixels(nStrips, lStrip, 20)
 theoStrip = np.zeros([nStrips*lStrip, 3])
 
-nBouncers=8
 bouncerList = []
-bouncerList.append(Bouncer(5, 0.1, (7,0,0), 64))
-bouncerList.append(Bouncer(5, 0.11, (6,0,1), 64))
-bouncerList.append(Bouncer(5, 0.12, (5,0,2), 64))
-bouncerList.append(Bouncer(5, 0.13, (4,0,3), 64))
-bouncerList.append(Bouncer(5, 0.14, (3,0,4), 64))
-bouncerList.append(Bouncer(5, 0.15, (2,0,5), 64))
-bouncerList.append(Bouncer(5, 0.16, (1,0,6), 64))
-bouncerList.append(Bouncer(5, 0.17, (0,0,7), 64))
+bouncerList.append(Bouncer(5, 0.1,  (7,0,0), 64))
+bouncerList.append(Bouncer(5, 0.101, (6,0,1), 64))
+bouncerList.append(Bouncer(5, 0.102, (5,0,2), 64))
+bouncerList.append(Bouncer(5, 0.103, (4,0,3), 64))
+bouncerList.append(Bouncer(5, 0.104, (3,0,4), 64))
+bouncerList.append(Bouncer(5, 0.105, (2,0,5), 64))
+bouncerList.append(Bouncer(5, 0.106, (1,0,6), 64))
+bouncerList.append(Bouncer(5, 0.107, (0,0,7), 64))
 
 
 while True:
-    for i in range(0,nBouncers):
-        stripNum = np.mod(i, nStrips)
+    for i in range(0,8):
+        stripNum = i
         base = stripNum*lStrip*2
         theoStrip[base:base+lStrip] = bouncerList[i].getFullOutArray()
-        base = stripNum*lStrip
-        theoStrip[base+lStrip:base+2*lStrip] = bouncerList[i].getFullOutArray()
+        base = stripNum*lStrip*2+lStrip
+        theoStrip[base+:base+lStrip] = bouncerList[i].getFullOutArray()
         bouncerList[i].update()
     pixels.update(theoStrip, 0.5, 0.5)
     #print((pixels.getArrayForDisplay())[0:64,0])
