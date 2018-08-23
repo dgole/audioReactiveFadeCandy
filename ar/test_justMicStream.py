d1437f79d3607580588bf150dc0110933cd54e69
dgole <dgole100@gmail.com>
2018-08-22 21:24:25 -0600

    Update test_justMicStream.py

--- ar/test_justMicStream.py
+++ ar/test_justMicStream.py
@@ -9,13 +9,10 @@ stream = micStream.Stream(fps=10, nBuffers=1)
 print(stream.fps)
 print(stream.framesPerBuffer)
 
-print("marker0")
-stream.readNewData()
-print("marker1")
-stream.readNewData()
-print("marker2")
-
-stream.stopStream()
+for i in range(0,10):
+    print("i=" + str(i))
+    stream.readNewData()
+    print("marker1")
 
 #print("marker0")
 #while True:
