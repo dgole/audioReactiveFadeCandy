e6556ae445b9269ffd472e7affd0c5ed8b6f4cfa
dgole <dgole100@gmail.com>
2018-08-23 15:24:18 -0600

    Create ar_bassBounce.py

---
+++ ar/ar_bassBounce.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+import micStream
+
+nStrips = 16
+lStrip  = 64
+client = fastopc.FastOPC('localhost:7890')
+
+pixels    = lib.Pixels(nStrips, lStrip, 0)
+theoStrip = np.zeros([lStrip, 3])
+
+stream = micStream.Stream(fps=40, nBuffers=4)
+
+powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
+nColorWheel = 1500
+colorWheel = lib.getColorWheel(nColorWheel)
+frameCount = 0
+
+while True:
+    success = stream.readAndCalc()
+    if success:
+        frameNumEff = np.mod(frameCount, nColorWheel)
+        power = np.sum(stream.freqSpectrum[10//5:300//5])
+        powerSmooth.update(power)
+        displayPower = int(122*power/powerSmooth.value)
+        theoStrip = displayPower * colorWheel[frameNumEff]
+        pixels.update(theoStrip, 0.9, 0.2)
+        #print(displayPower * colorWheel[frameNumEff])
+        client.putPixels(0, pixels.getArrayForDisplay())
+        frameCount+=1
