d91b43f9afc21529384254bd6e26e3c09cb023bb
dgole <dgole100@gmail.com>
2018-08-22 20:29:52 -0600

    Update test_justMicStream.py

--- ar/test_justMicStream.py
+++ ar/test_justMicStream.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-#import fastopc, time
+import fastopc, time
 import numpy as np
 #import functionLib as lib
 import micStream
 
-stream = micStream.Stream(fps=40, nBuffers=4)
+stream = micStream.Stream(fps=10, nBuffers=1)
 print(stream.fps)
 print(stream.framesPerBuffer)
+
+time.sleep(1)
+
+stream.stopStream()
 
 #print("marker0")
 #while True:
