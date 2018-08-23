41680ae8c3dcae81c695a799d2e67401799feb98
dgole <dgole100@gmail.com>
2018-08-23 15:26:36 -0600

    Update and rename ar_bassBounce.py to ar_bassScroll3.py

--- ar/ar_bassScroll3.py
+++ ar/ar_bassScroll3.py
@@ -10,12 +10,12 @@ lStrip  = 64
 client = fastopc.FastOPC('localhost:7890')
 
 pixels    = lib.Pixels(nStrips, lStrip, 0)
-theoStrip = np.zeros([lStrip, 3])
+theo      = np.zeros([nStrips*lStrip, 3])
 
 stream = micStream.Stream(fps=40, nBuffers=4)
 
 powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
-nColorWheel = 1500
+nColorWheel = 300
 colorWheel = lib.getColorWheel(nColorWheel)
 frameCount = 0
 
@@ -26,8 +26,9 @@ while True:
         power = np.sum(stream.freqSpectrum[10//5:300//5])
         powerSmooth.update(power)
         displayPower = int(122*power/powerSmooth.value)
-        theoStrip = displayPower * colorWheel[frameNumEff]
-        pixels.update(theoStrip, 0.9, 0.2)
+        theoStrip = np.roll(theoStrip, 1, axis=0)
+        theoStrip[0] = displayPower * colorWheel[frameNumEff]
+        pixels.update(theoStrip, 1.0, 0.5)
         #print(displayPower * colorWheel[frameNumEff])
         client.putPixels(0, pixels.getArrayForDisplay())
         frameCount+=1
