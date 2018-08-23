09478a56133a0560d32761c5c3306765ab37c9d8
dgole <dgole100@gmail.com>
2018-08-23 15:45:45 -0600

    Update ar_bassScroll4.py

--- ar/ar_bassScroll4.py
+++ ar/ar_bassScroll4.py
@@ -16,19 +16,29 @@ stream = micStream.Stream(fps=40, nBuffers=4)
 
 powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
 nColorWheel = 300
+nOnOneStrip = 20
 colorWheel = lib.getColorWheel(nColorWheel)
 frameCount = 0
 
 while True:
     success = stream.readAndCalc()
     if success:
-        frameNumEff = np.mod(frameCount, nColorWheel)
+        frameNumEff  = np.mod(frameCount, nColorWheel)
+        frameNumEff2 = np.mod(frameCount, nOnOneStrip*nStrips//2)
+        stripNum     = np.mod(frameNumEff, nStrips//2)
         power = np.sum(stream.freqSpectrum[10//5:300//5])
         powerSmooth.update(power)
         displayPower = int(122*power/powerSmooth.value)
-        theoStrip = np.roll(theoStrip, 128, axis=0)
-        theoStrip[0:128] = displayPower * colorWheel[frameNumEff]
-        pixels.update(theoStrip, 1.0, 0.5)
+        
+        theo = np.roll(theo, 1, axis=0)
+        
+        for i in range(nStrips):
+            theo[i*lStrip + 63] = [0,0,0]
+
+        theo[stripNum*2*lStrip] = displayPower * colorWheel[frameNumEff]
+        theo[stripNum*2*lStrip + lStrip] = displayPower * colorWheel[frameNumEff]
+        
+        pixels.update(theoStrip, 1.0, 1.0)
         #print(displayPower * colorWheel[frameNumEff])
         client.putPixels(0, pixels.getArrayForDisplay())
         frameCount+=1
