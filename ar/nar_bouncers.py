ec96c8bf19720574d3ade48dec710c4e1744d3be
dgole <dgole100@gmail.com>
2018-08-23 12:02:47 -0600

    Update nar_bouncers.py

--- ar/nar_bouncers.py
+++ ar/nar_bouncers.py
@@ -16,9 +16,7 @@ class Bouncer :
         self.n = n
         self.speed = speed
         self.color=color/np.amax(color)
-        self.color[0]=int(self.color[0]*255)
-        self.color[1]=int(self.color[1]*255)
-        self.color[2]=int(self.color[2]*255)
+        self.color=self.color*255
         print(np.amax(self.color))
     	self.nPixels = nPixels
         self.locInt = 10
