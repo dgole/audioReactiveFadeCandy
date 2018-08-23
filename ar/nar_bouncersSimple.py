36686b0c67c169a47be542169b89e24177cd8e81
dgole <dgole100@gmail.com>
2018-08-23 12:27:05 -0600

    Create nar_bouncersSimple.py

---
+++ ar/nar_bouncersSimple.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Light each LED in sequence, and repeat.
+
+import fastopc, time
+import numpy as np
+import functionLib as lib
+
+nStrips = 16
+lStrip  = 64
+
+client = fastopc.FastOPC('localhost:7890')
+
+class Bouncer :
+    def __init__(self, n, speed, color, nPixels):
+        self.n = n
+        self.speed = speed
+        self.color=color/np.amax(color)
+        self.color=self.color*255.0
+        for i in range(0,3): self.color[i]=int(self.color[i])
+        self.nPixels = nPixels
+        self.locInt = 10
+        self.locFloat = float(self.locInt)
+        self.outArray = np.zeros([nPixels, 3])
+        self.outZeros = np.zeros_like(self.outArray)
+        for i in range(self.locInt-self.n, self.locInt+self.n+1):
+            if i == self.locInt:
+                self.outArray[i] = self.color
+            else:
+                self.outArray[i] = self.color
+    def update(self):
+        self.locFloat = self.locFloat + self.speed
+        if int(self.locFloat) != self.locInt:
+            self.locInt = int(self.locFloat)
+            self.outArray = np.roll(self.outArray, int(np.sign(self.speed)), axis=0)
+        if self.locInt == self.n or self.locInt == self.nPixels-1-self.n:
+            self.speed = -self.speed            
+    def getFullOutArray(self):
+        returnArray = self.outArray 
+        return returnArray.astype(int)
+
+nBouncers = 32
+pixels = lib.Pixels(nStrips, lStrip, 20)
+theoStrip = np.zeros([nStrips*lStrip, 3])
+
+bouncerList = []
+for i in range(32): 
+    bouncerList.append(Bouncer(np.random.randint(1,high=8), np.random.rand()*0.3+0.05, np.random.rand(3), 64))
+
+
+while True:
+    for i in range(0,nBouncers):
+        stripNum = np.mod(i, nStrips)
+        base = stripNum*lStrip
+        theoStrip[base:base+lStrip] = bouncerList[i].getFullOutArray()
+        bouncerList[i].update()
+    pixels.update(theoStrip, 0.5, 0.5)
+    #print((pixels.getArrayForDisplay())[0:64,0])
+    client.putPixels(0, pixels.getArrayForDisplay())
+    #time.sleep(0.01)
+
+
