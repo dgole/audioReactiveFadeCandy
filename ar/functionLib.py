7536ed2079207ddc977827087cbd8ae7958438af
dgole <dgole100@gmail.com>
2018-08-23 14:13:13 -0600

    Update functionLib.py

--- ar/functionLib.py
+++ ar/functionLib.py
@@ -34,3 +34,24 @@ class Pixels():
         returnArray = self.array
         if self.minDisplay != 0: returnArray[returnArray < self.minDisplay] = 0
         return returnArray
+    
+    
+    
+class ExpFilter:
+    """Temporal exponential smoothing filter
+    """
+    def __init__(self, val=0.0, alpha_decay=0.5, alpha_rise=0.5):
+        """Small rise / decay factors = more smoothing"""
+        self.alpha_decay = alpha_decay
+        self.alpha_rise = alpha_rise
+        self.value = val
+    def update(self, value):
+        if isinstance(self.value, (list, np.ndarray, tuple)):
+            alpha = value - self.value
+            alpha[alpha > 0.0] = self.alpha_rise
+            alpha[alpha <= 0.0] = self.alpha_decay
+        else:
+            alpha = self.alpha_rise if value > self.value else self.alpha_decay
+        self.value = alpha * value + (1.0 - alpha) * self.value
+    
+    
