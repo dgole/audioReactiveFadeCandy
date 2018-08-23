90a7c37f26289c5a3003049793a5735652d32bfa
dgole <dgole100@gmail.com>
2018-08-23 14:56:01 -0600

    Update ar_bassThumpAllSame.py

--- ar/ar_bassThumpAllSame.py
+++ ar/ar_bassThumpAllSame.py
@@ -7,7 +7,7 @@ import micStream
 
 nStrips = 16
 lStrip  = 64
-client = fastopc.FastOPC('localhost:7890')
+#client = fastopc.FastOPC('localhost:7890')
 
 pixels    = lib.Pixels(nStrips, lStrip, 0)
 theoStrip = np.zeros([lStrip, 3])
@@ -15,14 +15,19 @@ theoStrip = np.zeros([lStrip, 3])
 stream = micStream.Stream(fps=40, nBuffers=4)
 
 powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
+nColorWheel = 1500
+colorWheel = lib.getColorWheel(nColorWheel)
+frameCount = 0
 
 while True:
     success = stream.readAndCalc()
-    if success: 
+    if success:
+        frameNumEff = np.mod(frameCount, nColorWheel)
         power = np.sum(stream.freqSpectrum[10//5:300//5])
         powerSmooth.update(power)
         displayPower = int(122*power/powerSmooth.value)
-        theoStrip[:,0] = displayPower
+        theoStrip[:,0] = displayPower * colorWheel[frameNumEff]
         pixels.update(theoStrip, 0.9, 0.2)
-        #print(displayPower)
-        client.putPixels(0, pixels.getArrayForDisplay())
+        print(displayPower * colorWheel[frameNumEff])
+        #client.putPixels(0, pixels.getArrayForDisplay())
+        frameCount+=1
