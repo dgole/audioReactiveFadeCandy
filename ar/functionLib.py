812f99f024db5a936444a185a4da03a2a2202072
dgole <dgole100@gmail.com>
2018-08-23 14:51:46 -0600

    Update functionLib.py

--- ar/functionLib.py
+++ ar/functionLib.py
@@ -55,3 +55,20 @@ class ExpFilter:
         self.value = alpha * value + (1.0 - alpha) * self.value
     
     
+def getColorWheel(nTot):
+    colorWheel = np.zeros([3, nTot])
+    nTot3 = nTot//3
+    for n in range(nTot):
+        if n < nTot3:
+            colorWheel[0,n] = 1.0 - float(n)/float(nTot3)
+            colorWheel[1,n] = 0.0 + float(n)/float(nTot3)
+            colorWheel[2,n] = 0.0
+        elif nTot3 < n < 2*nTot3:
+            colorWheel[0,n] = 0.0
+            colorWheel[1,n] = 1.0 - float(n-nTot3)/float(nTot3)
+            colorWheel[2,n] = 0.0 + float(n-nTot3)/float(nTot3)
+        elif 2*nTot3 < n < nTot:
+            colorWheel[0,n] = 0.0 + float(n-2*nTot3)/float(nTot3)
+            colorWheel[1,n] = 0.0
+            colorWheel[2,n] = 1.0 - float(n-2*nTot3)/float(nTot3)
+    return colorWheel
