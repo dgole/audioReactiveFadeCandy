3f1bff70893531fc1fcdb836c195cea958ee21f2
dgole <dgole100@gmail.com>
2018-08-23 19:02:50 -0600

    Update ar_bassScroll4.py

--- ar/ar_bassScroll4.py
+++ ar/ar_bassScroll4.py
@@ -12,7 +12,7 @@ client = fastopc.FastOPC('localhost:7890')
 pixels    = lib.Pixels(nStrips, lStrip, 0)
 theo      = np.zeros([nStrips*lStrip, 3])
 
-stream = micStream.Stream(fps=40, nBuffers=4)
+stream = micStream.Stream(fps=30, nBuffers=4)
 
 powerSmooth = lib.ExpFilter(val=0.05, alpha_rise=0.05, alpha_decay=0.05)
 nColorWheel = 300
@@ -28,7 +28,7 @@ while True:
         stripNum     = np.mod(frameNumEff, nStrips//2)
         power = np.sum(stream.freqSpectrum[10//5:300//5])
         powerSmooth.update(power)
-        displayPower = int(122*power/powerSmooth.value)
+        displayPower = int(122*np.power(power/powerSmooth.value,1.5))
         
         theo = np.roll(theo, 1, axis=0)
         
@@ -38,7 +38,7 @@ while True:
         theo[stripNum*2*lStrip] = displayPower * colorWheel[frameNumEff]
         theo[stripNum*2*lStrip + lStrip] = displayPower * colorWheel[frameNumEff]
         
-        pixels.update(theoStrip, 1.0, 1.0)
+        pixels.update(theo, 1.0, 1.0)
         #print(displayPower * colorWheel[frameNumEff])
         client.putPixels(0, pixels.getArrayForDisplay())
         frameCount+=1
