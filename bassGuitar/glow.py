2980815b1ae9400bece536a62a5ff0a23038bd46
dgole <dgole100@gmail.com>
2018-08-29 10:59:18 -0600

    Update glow.py

--- bassGuitar/glow.py
+++ bassGuitar/glow.py
@@ -1,1 +1,21 @@
-a
+#
+
+
+
+# start with 4 strips
+# 1. around body (40 leds)
+# 2. along neck top (20 leds)
+# 3. along neck bottom (20 leds)
+# 4. along strap (40ish)
+# 4 fadecandy channels
+# that's about 120 leds total
+# 120 * 20 mA per color led = 2.4 A, exactly max for battery.  
+# dial fadecandy whitepoint to 0.8 or so and we're good to max out 1 color.
+# 
+# MODE IDEAS
+# body brightness all same, scales with volume.  number of lights lit on neck scales with volume. 
+#
+#
+#
+#
+#
